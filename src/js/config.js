c3ef5599f404b4cec106bfe87083474058dbdeee
Martin Kamír <martin.k.kamir@gmail.com>
2023-05-28 19:38:05 +0200

    test

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const TIMEOUT_SEC = 10;
 export const API_URL_UNSPLASH = 'https://api.unsplash.com/photos/random';
 export const API_URL_GEOAPIFY = 'https://api.geoapify.com/v1/geocode/reverse';
 export const API_URL_QUOTABLE = 'https://api.quotable.io/quotes/random';
-export const API_URL_IPAPI = 'http://ip-api.com/json';
+export const API_URL_IPAPI = 'https://ip-api.com/json';
